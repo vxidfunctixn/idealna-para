8d957f5c0acea3e007d4d3c396f869432bd10ea9
Marcin Kalinowski <marcin36k@outlook.com>
2021-03-07 08:57:54 +0100

    Add theme switcher and simplify searchbox

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
-const bodyEl = document.getElementsByTagName('body')
+const bodyEl = document.getElementsByTagName('body')[0]
 const searchFormEl = document.getElementById('search-form')
 const searchInputEl = document.getElementById('search-input')
+const searchSubmitEl = document.getElementById('search-submit')
+const invertColorsLabelEl = document.getElementById('invert-colors-label')
 
 const toggleNav = () => {
-  bodyEl[0].classList.toggle('expand-nav')
+  bodyEl.classList.toggle('expand-nav')
 }
 
 const showSearch = () => {
@@ -11,6 +13,26 @@ const showSearch = () => {
   searchInputEl.focus()
 }
 
-const closeSearch = () => {
+searchInputEl.addEventListener('blur', (e) => closeSearch(e))
+searchSubmitEl.addEventListener('blur', (e) => closeSearch(e))
+const closeSearch = (e) => {
+  const { relatedTarget } = e
+  if (
+    relatedTarget &&
+    (relatedTarget.classList.contains('search-button') ||
+      relatedTarget.classList.contains('search-input'))
+  )
+    return
   searchFormEl.classList.remove('show')
+}
+
+const invertColors = () => {
+  const theme = bodyEl.getAttribute('theme')
+  if (theme === 'light') {
+    bodyEl.setAttribute('theme', 'dark')
+    invertColorsLabelEl.innerText = 'Tryb jasny'
+  } else {
+    bodyEl.setAttribute('theme', 'light')
+    invertColorsLabelEl.innerText = 'Tryb ciemny'
+  }
 }
